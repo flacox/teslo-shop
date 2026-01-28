b11eb0d7ed93c683562e41519b33802ba59c3a24
flacox <anderson0430@gmail.com>
2026-01-28 00:03:46 -0400

    images added and product section

--- src/shop/pages/home/HomePage.tsx
+++ src/shop/pages/home/HomePage.tsx
@@ -1,10 +1,17 @@
+import { CustomPagination } from "@/components/custom/CustomPagination"
+import { products } from "@/moks/products.mock"
 import { CustomJombotron } from "@/shop/components/CustomJombotron"
+import { ProductsGrid } from "@/shop/components/ProductsGrid"
 
 
 export const HomePage = () => {
   return (
     <>
       <CustomJombotron title="Todos los productos"/>
+
+      <ProductsGrid products={products} />
+
+      <CustomPagination totalPages={7} />
     </>
   )
 }
